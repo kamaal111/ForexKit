2a48cceb91e8c4430648296b2d281b883b9f2992
Kamaal <kamaal.f1@gmail.com>
2023-02-15 21:36:04 +0100

    Making base url configurable

--- Sources/ForexKit/Models/ForexKitConfiguration.swift
+++ Sources/ForexKit/Models/ForexKitConfiguration.swift
@@ -13,17 +13,20 @@ public class ForexKitConfiguration {
     let urlSession: URLSession
     var container: CacheContainerable
     let ratePointPrecision: Int
+    let baseURL: URL
 
     public init(
         preview: Bool = false,
         skipCaching: Bool = false,
         urlSession: URLSession = .shared,
         container: CacheContainerable? = nil,
-        ratePointPrecision: Int = 4) {
+        ratePointPrecision: Int = 4,
+        baseURL: URL = URL(staticString: "https://theforexapi.com/api")) {
             self.skipCaching = skipCaching
             self.preview = preview
             self.urlSession = urlSession
             self.ratePointPrecision = ratePointPrecision
+            self.baseURL = baseURL
             if let container {
                 self.container = container
             } else {
